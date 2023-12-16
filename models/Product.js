199f9501b5869bb407e3049cf8b577d59d3a2d20
Nate McMahon <nathaniel.mcmahon@hrblock.com>
2023-12-15 23:43:16 -0600

    Set up routes

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,39 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        // This references the `category` model, which we set in `Category.js` as its `modelName` property
+        model: 'category',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,28 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        // This references the `product` model, which we set in `Product.js` as its `modelName` property
+        model: 'product',
+        key: 'id',
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        // This references the `tag` model, which we set in `Tag.js` as its `modelName` property
+        model: 'tag',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
